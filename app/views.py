efc5ebfa4a7f29125ef393668505e8a4547766b2
Inae Capanna <icapannasoriani@gmail.com>
2023-08-16 09:23:00 -0300

    atualizar

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, "app/index.html")
+
+def sobre(request):
+    return render(request, "app/sobre.html")
+
+def equipe(request):
+    return render(request, "app/equipe.html")
+
